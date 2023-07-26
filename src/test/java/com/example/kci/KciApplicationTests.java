9ba9bdfff9103b626ce131327d9fd07f6d532a79
choengilkim <clamp222@naver.com>
2023-07-26 14:06:50 +0900

    세팅

---
+++ src/test/java/com/example/kci/KciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
