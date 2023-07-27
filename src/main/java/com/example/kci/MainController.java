98727450f82c0c93768c2d40754e5aa130854edb
choengilkim <clamp222@naver.com>
2023-07-27 13:25:22 +0900

    구구단 응답, 스트림방식으로 구현

--- src/main/java/com/example/kci/MainController.java
+++ src/main/java/com/example/kci/MainController.java
@@ -3,14 +3,19 @@ package com.example.kci;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 public class MainController {
 
+  private int increaseNo = -1;
   @RequestMapping("/kci")
   @ResponseBody
   public String index() {
     return "안녕하세요.";
   }
+
   @GetMapping("/page1")
   @ResponseBody
   public String showGet() {
@@ -21,6 +26,7 @@ public class MainController {
            </form>
            """;
   }
+
   @PostMapping("/page2")
   @ResponseBody
   public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
@@ -29,6 +35,7 @@ public class MainController {
            <h1>안녕하세요. POST 방식으로 오신걸 환영합니다.</h1>
            """.formatted(age);
   }
+
   @GetMapping("/page2")
   @ResponseBody
   public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
@@ -36,5 +43,41 @@ public class MainController {
            <h1>입력된 나이 : %d</h1>
            <h1>안녕하세요. POST 방식으로 오신걸 환영합니다.</h1>
            """.formatted(age);
+  }
+
+  @GetMapping("/plus")
+  @ResponseBody
+  public int showPlus(int a, int b) {
+    return a + b;
+  }
+
+  @GetMapping("/minus")
+  @ResponseBody
+  public int showMinus(int a, int b) {
+    return a - b;
+  }
+
+  @GetMapping("/increase")
+  @ResponseBody
+  public int showIncrease() {
+    increaseNo++;
+    return increaseNo;
+  }
+
+  @GetMapping("/gugudan")
+  @ResponseBody
+  public String showIncrease(Integer dan, Integer limit) {
+    if(dan == null) {
+      dan = 9;
+    }
+
+    if(limit == null) {
+      limit = 9;
+    }
+
+    Integer finalDan = dan;
+    return IntStream.rangeClosed(1, limit)
+        .mapToObj(i -> "%d * %d =%d".formatted(finalDan, i, finalDan*i))
+        .collect(Collectors.joining("<br>\n"));
   }
 }
