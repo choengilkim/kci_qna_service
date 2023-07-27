323f6b200ff49d329e34d3cab7fef36180fd94cb
choengilkim <clamp222@naver.com>
2023-07-27 12:47:28 +0900

    Getmapping,PostMapping, RequestParam

--- src/main/java/com/example/kci/MainController.java
+++ src/main/java/com/example/kci/MainController.java
@@ -13,7 +13,7 @@ public class MainController {
   }
   @GetMapping("/page1")
   @ResponseBody
-  public String showMain() {
+  public String showGet() {
     return """
            <form method="POST" action="/page2">
               <input type="number" name="age" placeholder="나이 입력" />
@@ -23,7 +23,15 @@ public class MainController {
   }
   @PostMapping("/page2")
   @ResponseBody
-  public String showPost(@RequestParam(defaultValue = "0") int age) {
+  public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
+    return """
+           <h1>입력된 나이 : %d</h1>
+           <h1>안녕하세요. POST 방식으로 오신걸 환영합니다.</h1>
+           """.formatted(age);
+  }
+  @GetMapping("/page2")
+  @ResponseBody
+  public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
     return """
            <h1>입력된 나이 : %d</h1>
            <h1>안녕하세요. POST 방식으로 오신걸 환영합니다.</h1>
